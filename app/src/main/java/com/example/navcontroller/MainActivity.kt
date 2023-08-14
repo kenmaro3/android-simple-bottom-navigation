c15200b3b19e6593f380186f55674b7afb2a764e
kenmaro3 <miha.ken.19@gmail.com>
2023-08-14 11:45:56 +0900

    initial commit

---
+++ app/src/main/java/com/example/navcontroller/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.navcontroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navcontroller.View.Home
+import com.example.navcontroller.View.Profile
+import com.example.navcontroller.ui.theme.NavControllerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavControllerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ){
+
+                        composable(route = "home") {
+                            Scaffold(
+                                bottomBar = {
+                                    BottomBar(
+                                        navController = navController
+                                    )
+                                },
+                            ) { contentPadding ->
+
+                                Home()
+                            }
+
+                        }
+
+                        composable(route="profile"){
+                            Scaffold(
+                                bottomBar = {
+                                    BottomBar(
+                                        navController = navController
+                                    )
+                                },
+                            ) { contentPadding ->
+                                Profile()
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
